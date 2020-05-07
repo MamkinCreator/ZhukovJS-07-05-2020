05262163b66b366156bf9376011d704abbd48182
MamkinCreator <52317810+MamkinCreator@users.noreply.github.com>
2020-05-07 13:10:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 require('http')
 .Server((req, res) => {
-    if (req.url === '/json') {
-      res.writeHead(200, {
-         'Content-Type': 'application/json; charset=utf-8',
-         'Access-Control-Allow-Origin': '*'
-      });
-
-      res.end('{"result": "правильный JSON"}');
-    }
-
-    if (req.url === '/zero') {
-      res.writeHead(404, {
-         'Content-Type': 'text/html; charset=utf-8'
-      });
-
-      res.end('<h1>Привет герценовцам!</h1>');
-    }
-    res.writeHead(200, {
-      'Content-Type': 'text/javascript; charset=utf-8'
-    });
-    res.end('<h1>Жуков Сергей</h1>','<h2></h2>');
+    res.end('<h1>Жуков Сергей</h1>');
  })
 .listen(process.env.PORT);
