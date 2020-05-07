fed36d9a0daff5868193890161f86ebef2cafd41
MamkinCreator <52317810+MamkinCreator@users.noreply.github.com>
2020-05-07 13:04:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,17 +16,9 @@ require('http')
 
       res.end('<h1>Привет герценовцам!</h1>');
     }
-    if (req.url === '') {
-      res.writeHead(200, {
-         'Content-Type': 'application/json; charset=utf-8',
-         'Access-Control-Allow-Origin': '*'
-      });
-
-      res.end('<h1>Жуков Сергей</h1>','<h2></h2>');
-    }
     res.writeHead(200, {
       'Content-Type': 'text/javascript; charset=utf-8'
     });
-    require('fs').createReadStream('./index.js').pipe(res);
+    res.end('<h1>Жуков Сергей</h1>','<h2></h2>');
  })
 .listen(process.env.PORT);
