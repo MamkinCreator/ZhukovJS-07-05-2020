0b4df4c5dabb51d6e4f3888ff80ebdf79a4fdf37
MamkinCreator <52317810+MamkinCreator@users.noreply.github.com>
2020-05-07 13:20:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('http')
 .Server((req, res) => {
-    res.send('<h1>Жуков Сергей</h1>');
+    res.writeHead(200, {
+         'Content-Type': 'application/json; charset=utf-8',
+      });
+    res.end('<h1>Жуков Сергей</h1>');
  })
 .listen(process.env.PORT);
