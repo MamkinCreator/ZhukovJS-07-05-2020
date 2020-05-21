ca4a9c2da87995dc04150543f6fc75a26536a073
MamkinCreator <52317810+MamkinCreator@users.noreply.github.com>
2020-05-21 17:25:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ require('http')
 .Server((req, res) => {
     res.setHeader('Content-Type','text/html; charset=utf-8')
     const now = moment().format('DD/MM/Y H:mm');
-    res.end('<h1>Zhukov Sergey</h1> <h2>${now}<h2>');
+    res.end('<h1>Zhukov Sergey</h1><h2>${now}<h2>');
  })
 .listen(process.env.PORT);
